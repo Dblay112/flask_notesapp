53edcc81ae3945433782a3b85fdff044766dda1e
Bright <brightdjogatse@gmail.com>
2024-07-09 14:36:14 +0000

    created database and corrected __init__.py errors

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os  # Import the os module
+from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -8,8 +9,10 @@ DB_NAME = 'database.db'
 def create_app():
     """Function to create and initialize the Flask app"""
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Hard to guess'  # Secures the cookies and session data
+    app.config['SECRET_KEY'] = 'Hard to guess'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -20,9 +23,18 @@ def create_app():
     from .models import User, Note
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app
 
 def create_database(app):
-    if not os.path.exists('website/' + DB_NAME):  # Use os.path.exists
-        db.create_all(app=app)
+    if not os.path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
         print('Database Created Successfully')
